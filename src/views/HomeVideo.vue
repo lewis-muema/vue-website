569de69862aba24535054acf8bbc3abc710f71d1
lewis muema <lewis@sendy.co.ke>
2018-11-29 22:41:59 +0300

    fix alignment issues with different resolutions and switch the images for home video section

--- src/views/HomeVideo.vue
+++ src/views/HomeVideo.vue
@@ -29,15 +29,15 @@
             </div>
 </div>
 
-<div class="video-section absolute overflow" v-if="windowWidth > '768'" :style= "{right: newLeft + '%' , width: newWidth + '%'}">
+<div class="video-section absolute overflow" v-if="windowWidth > '768'" :style= "{right: newRight + '%' , width: newWidth + '%'}">
     <div class="phone-video-section absolute">
         <img class="home-video-show-mobile-image absolute" src="https://images.sendyit.com/website/home/mobile_frame_01.png?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16">
-        <img class="home-video-show-mobile-video absolute" src="https://s3-eu-west-1.amazonaws.com/images.sendyit.com/website/home2/mobile.png">
+        <img class="home-video-show-mobile-video absolute" src="https://s3-eu-west-1.amazonaws.com/images.sendyit.com/website/home2/mobile.jpg">
             <!--<video class="home-video-show-mobile-video absolute" loop="yes" muted="" autoplay="true" playsinline="" preload="true" src="https://images.sendyit.com/website/home/mobile_video_01.mp4?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16" poster="https://images.sendyit.com/website/home/mobile_first_image_01.png?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16" width="164.347826087px" __idm_id__="140516354"></video>-->
     </div>
     <div class="desktop-video-section absolute">
         <img class="home-video-show-desktop-image absolute" src="https://images.sendyit.com/website/home/laptop_frame_01.png?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16">
-        <img class="home-video-show-desktop-video absolute" src="https://s3-eu-west-1.amazonaws.com/images.sendyit.com/website/home2/laptop.png">
+        <img class="home-video-show-desktop-video absolute" src="https://s3-eu-west-1.amazonaws.com/images.sendyit.com/website/home2/laptop.jpg">
             <!--<video class="home-video-show-desktop-video absolute" loop="yes" muted="" autoplay="true" playsinline="" preload="true" src="https://images.sendyit.com/website/home/laptop_video_01.mp4?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16" poster="https://images.sendyit.com/website/home/laptop_first_image_01.png?v=1LeOeCUTAAAAAGs99manIwc7kghOUdgkr_rnuoCE16" width="687.4183976261px" __idm_id__="140516353"></video>-->
             
     </div>
@@ -103,7 +103,15 @@ created() {
         var quotient = (range * 13.5)/1536
         this.newLeft = 19 - quotient
         var quotient2 = (range * 19)/1120
-        this.newWidth = 35 + quotient2
+        if(this.windowWidth > 1200){
+            this.newWidth = (35 + quotient2) + 3
+            this.newRight = this.newLeft - 3
+        }
+        else{
+            this.newWidth = 35 + quotient2
+            this.newRight = this.newLeft
+        }
+        
     }
     }
 }
