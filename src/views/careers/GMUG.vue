d437ccdf521d626a7e72fda2386f6a328899dfe4
lewis muema <lewis@sendy.co.ke>
2018-12-07 14:13:30 +0300

    fix spacing on GM page

--- src/views/careers/GMUG.vue
+++ src/views/careers/GMUG.vue
@@ -3,7 +3,7 @@
         <Upper/>
         <div class="job-desc" :style= "{marginLeft: newLeft + '%' , marginRight: newLeft + '%'}">
             <p class="job-desc-head">COUNTRY MANAGER UGANDA</p>
-            <p class="jobs-listing">Logistics in Africa is broken! It’s super fragmented, informal and very costly, making it difficult forsmall business to trade in Africa. At Sendy we are on a mission to solve this problem. We have built the first efficient shared logistics platform for Africa that will create new possibilities forsmall businesses and transporters.<br><br>
+            <p class="jobs-listing">Logistics in Africa is broken! It’s super fragmented, informal and very costly, making it difficult for small business to trade in Africa. At Sendy we are on a mission to solve this problem. We have built the first efficient shared logistics platform for Africa that will create new possibilities for small businesses and transporters.<br><br>
             A Country Manager is an executive level position which oversees all of Sales, Partner Operations, & Support at a country level and reports directly to the Commercial Director.
             </p>
             <p class="job-desc-head">ABOUT THE ROLE</p>
